fd4573d4ddd8100a4c2f8faca9ce61266a53f65e
tharindua <ghtd.dharmarathna@gmail.com>
2015-08-23 19:06:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# customAccessTokenHandler
+This handler can use to send access token of API in WSO2 API Manager as a query parameter instead of Header
---
+++ src/main/java/com/wso2/carbon/apimgt/custom/handler/CustomAccessTokenHandler.java
@@ -0,0 +1,78 @@
+package com.wso2.carbon.apimgt.custom.handler;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.synapse.ManagedLifecycle;
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.core.SynapseEnvironment;
+import org.apache.synapse.core.axis2.Axis2MessageContext;
+import org.apache.synapse.rest.AbstractHandler;
+import org.apache.synapse.transport.nhttp.NhttpConstants;
+import org.apache.synapse.util.xpath.SynapseXPath;
+import org.jaxen.JaxenException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by tharindu on 8/2/15.
+ */
+public class CustomAccessTokenHandler extends AbstractHandler implements ManagedLifecycle {
+	private static final Log log = LogFactory.getLog(CustomAccessTokenHandler.class);
+
+	@Override
+	public boolean handleRequest(MessageContext messageContext) {
+		org.apache.axis2.context.MessageContext axis2MC =
+				((Axis2MessageContext) messageContext).getAxis2MessageContext();
+		String accessToken = "";
+		try {
+			accessToken = new SynapseXPath("$url:access-token").stringValueOf(messageContext);
+		} catch (JaxenException e) {
+			log.error("couldn't found token in query parameters", e);
+		}
+
+		//remove the query parameter
+
+		String rest_url_postfix = (String) axis2MC.getProperty(NhttpConstants.REST_URL_POSTFIX);
+		rest_url_postfix = removeTokenFromQueryParameters(rest_url_postfix, accessToken);
+		axis2MC.setProperty(NhttpConstants.REST_URL_POSTFIX, rest_url_postfix);
+
+		//set authorization bearer header to use for the api authentication
+
+		Map headers =
+				(Map) axis2MC.getProperty(org.apache.axis2.context.MessageContext.TRANSPORT_HEADERS);
+		if (headers != null) {
+			headers.put("Authorization", "Bearer " + accessToken);
+
+		} else {
+			headers = new HashMap<String, String>();
+			headers.put("Authorization", "Bearer " + accessToken);
+			axis2MC.setProperty(org.apache.axis2.context.MessageContext.TRANSPORT_HEADERS, headers);
+		}
+		return true;
+	}
+
+	@Override
+	public boolean handleResponse(MessageContext messageContext) {
+		return true;
+	}
+
+	@Override
+	public void init(SynapseEnvironment synapseEnvironment) {
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+	private String removeTokenFromQueryParameters(String input, String accessToken) {
+		input = input.replace("?access-token=" + accessToken, "?");
+		input = input.replace("&access-token=" + accessToken, "");
+		input = input.replace("?&", "?");
+		if (input.lastIndexOf("?") == (input.length() - 1)) {
+			input = input.replace("?", "");
+		}
+		return input;
+	}
+}
